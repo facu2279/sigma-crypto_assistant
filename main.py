dff86b7a7fb30aa760e596ee9610d381f28c2439
Ubuntu <ubuntu@sigma.cryptocurrency.assistant@gmail.com>
2021-05-29 14:23:01 +0000

    Fixing datebase

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+#!/usr/bin/python3
+"""
+Made by Facundo Diaz - Tomas De Castro - Tadeo Grach for Holberton School 2021 """
+
 """
 color fondo en hexa #262833
 color logo en hexa es #39E991
 """
+
+
+""" IMPORTS """
+import persistence
+import info
+
+url = "https://api.binance.com/"
+print("empezo")
+for i in range(0,20):
+    price = info.consultar_precio_BTC(url)
+    persistence.save_price_bitcoin(price)
+print("termino")
--- persistence.py
+++ persistence.py
@@ -7,7 +7,7 @@ MY_H = "localhost"
 MY_U = "root"
 MY_P = "betty"
 MY_D = "sigma"
-nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+"""nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
 consulta = nuevaconexion.cursor()
 consulta.execute("SELECT * FROM coins;")
 resultado = consulta.fetchall()
@@ -15,4 +15,10 @@ print(len(resultado))
 for fila in resultado:
             print(fila)
 print(type(fila))
-nuevaconexion.close()
+nuevaconexion.close()"""
+
+def save_price_bitcoin(price):
+            nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+            consulta = nuevaconexion.cursor()
+            consulta.execute("INSERT INTO history_btc (name, price) VALUES ('BTC', " + str(price) + ");")
+            nuevaconexion.commit()
