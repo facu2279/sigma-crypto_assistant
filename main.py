8eb476fe3be3ce9fff8aac9f8564b174e0f8f832
sigma123server <ubuntu@sigma.cryptocurrency.assistant@gmail.com>
2021-05-30 19:41:33 +0000

    test

--- main.py
+++ main.py
@@ -2,19 +2,28 @@
 """
 Made by Facundo Diaz - Tomas De Castro - Tadeo Grach for Holberton School 2021 """
 
-"""
-color fondo en hexa #262833
-color logo en hexa es #39E991
-"""
+""" colour background in hexa #262833 colour of logo in hexa #39E991 """
 
+""" IMPORTS EXTERN MODULES """
+import time
 
-""" IMPORTS """
+""" IMPORTS FILES """
 import persistence
 import info
 
+""" In case url 1 is not changing for these
+url = "https://api2.binance.com/"
+url = "https://api3.binance.com/"
+"""
 url = "https://api.binance.com/"
+
+
 print("empezo")
-for i in range(0,20):
-    price = info.consultar_precio_BTC(url)
-    persistence.save_price_bitcoin(price)
+for i in range(0,300):
+        btc = info.consultar_precio_BTC(url)
+        persistence.save_price_bitcoin(btc)
+        """eth = info.consultar_precio_ETH(url)
+        persistence.save_price_ethereum(eth)"""
+        doge = info.consultar_precio_DOGE(url)
+        persistence.save_price_doge(doge)
 print("termino")
--- persistence.py
+++ persistence.py
@@ -7,18 +7,120 @@ MY_H = "localhost"
 MY_U = "root"
 MY_P = "betty"
 MY_D = "sigma"
-"""nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
-consulta = nuevaconexion.cursor()
-consulta.execute("SELECT * FROM coins;")
-resultado = consulta.fetchall()
-print(len(resultado))
-for fila in resultado:
-            print(fila)
-print(type(fila))
-nuevaconexion.close()"""
 
+
+""" btc """
 def save_price_bitcoin(price):
-            nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
-            consulta = nuevaconexion.cursor()
-            consulta.execute("INSERT INTO history_btc (name, price) VALUES ('BTC', " + str(price) + ");")
-            nuevaconexion.commit()
+    nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+    consulta = nuevaconexion.cursor()
+    consulta.execute("SELECT count(*) from history_btc;")
+    resultado = consulta.fetchall()
+    for i in resultado:
+        for i2 in i:
+            cant_items = i2
+    if cant_items < 288:
+        insertar_btc(str(price))
+    else:
+        borrar_ultimo_btc(str(price))
+
+def insertar_btc(price):
+    nueva = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+    consulta = nueva.cursor()
+    consulta.execute("INSERT INTO history_btc (name, price) VALUES ('BTC', " + str(price) + ");")
+    nueva.commit()
+
+def borrar_ultimo_btc(price):
+    newxd = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+    consulta = newxd.cursor()
+    consulta.execute("select id from history_btc ORDER BY id ASC limit 1;")
+    resultado = consulta.fetchall()
+    for i in resultado:
+        for i2 in i:
+            id = i2
+            id = str(id)
+            borrar_item(id, str(price))
+            
+def borrar_item(id, price):
+    nuevaxd = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+    consulta = nuevaxd.cursor()
+    consulta.execute("DELETE FROM history_btc WHERE id ="+ id)
+    nuevaxd.commit()
+    insertar_btc(str(price))
+    
+""" doge """
+
+def save_price_doge(price):
+        nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+        consulta = nuevaconexion.cursor()
+        consulta.execute("SELECT count(*) from history_doge;")
+        resultado = consulta.fetchall()
+        for i in resultado:
+            for i2 in i:
+                cant_items = i2
+        if cant_items < 288:
+            insertar_doge(str(price))
+        else:
+            borrar_ultimo_doge(str(price))
+
+def insertar_doge(price):
+    nueva = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+    consulta = nueva.cursor()
+    consulta.execute("INSERT INTO history_doge (name, price) VALUES ('DOGE', " + str(price) + ");")
+    nueva.commit()
+
+def borrar_ultimo_doge(price):
+    newxd = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+    consulta = newxd.cursor()
+    consulta.execute("select id from history_doge ORDER BY id ASC limit 1;")
+    resultado = consulta.fetchall()
+    for i in resultado:
+        for i2 in i:
+            id = i2
+            id = str(id)
+            borrar_item(id, str(price))
+
+def borrar_item(id, price):
+    nuevaxd = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+    consulta = nuevaxd.cursor()
+    consulta.execute("DELETE FROM history_doge WHERE id ="+ id)
+    nuevaxd.commit()
+    insertar_doge(str(price))
+
+""" ethereum """
+
+def save_price_ethereum(price):
+    nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+    consulta = nuevaconexion.cursor()
+    consulta.execute("SELECT count(*) from history_eth;")
+    resultado = consulta.fetchall()
+    for i in resultado:
+        for i2 in i:
+            cant_items = i2
+    if cant_items < 288:
+        insertar_eth(str(price))
+    else:
+        borrar_ultimo_eth(str(price))
+
+def insertar_eth(price):
+    nueva = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+    consulta = nueva.cursor()
+    consulta.execute("INSERT INTO history_eth (name, price) VALUES ('ETH', " + str(price) + ");")
+    nueva.commit()
+
+def borrar_ultimo_eth(price):
+    newxd = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+    consulta = newxd.cursor()
+    consulta.execute("select id from history_eth ORDER BY id ASC limit 1;")
+    resultado = consulta.fetchall()
+    for i in resultado:
+        for i2 in i:
+            id = i2
+            id = str(id)
+            borrar_item(id, str(price))
+
+def borrar_item(id, price):
+    nuevaxd = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
+    consulta = nuevaxd.cursor()
+    consulta.execute("DELETE FROM history_eth WHERE id ="+ id)
+    nuevaxd.commit()
+    insertar_eth(str(price))
