559de483507e2aa65f8f577b2dbd6c964a45dfd1
facu2279 <2279@holbertonschool.com>
2021-05-30 18:16:05 -0300

    Update process.py

--- web_dynamic/process.py
+++ web_dynamic/process.py
@@ -5,9 +5,12 @@ Made by Facundo Diaz - Tomas De Castro - Tadeo Grach for Holberton School 2021 "
 """ colour background in hexa #262833 colour of logo in hexa #39E991 """
 
 """ IMPORTS EXTERN MODULES """
+import sys
+# sys.path is a list of absolute path strings
+sys.path.append('/home/ubuntu/proyecto_final')
 
+import persistence
 """ IMPORTS FILES """
-import proyecto_final.persistence
 
 persistence.insert_new_user("test", "correofd")
 print("alo")
