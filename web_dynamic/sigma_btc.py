c5d2571c6661edc97043565b46ad0434d122700f
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-05-31 14:11:09 -0300

    Update sigma_btc.py

--- web_dynamic/sigma_btc.py
+++ web_dynamic/sigma_btc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ """
-from process import btc
+import process
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -9,9 +9,9 @@ app = Flask(__name__)
 @app.route('/bitcoin', strict_slashes=False)
 def bitcoin():
     """ """
-    price = btc.price
-    name = btc.name
-    suggest = btc.suggest
+    price = process.btc.price
+    name = process.btc.name
+    suggest = process.btc.suggest
     render_template('bitcoin.html', price=price, name=name, suggest=suggest)
 
 if __name__ == "__main__":
