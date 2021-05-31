4235cc3e4352e6784d1af7c217ddf293a5e203ed
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-05-31 14:37:08 -0300

    Create mail.py

---
+++ web_dynamic/mail.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Made by Facundo Diaz - Tomas De Castro - Tadeo Grach for Holberton School 2021 """
+
+""" IMPORTS EXTERN MODULES """
+import sys
+import os
+sys.path.append('/home/ubuntu/proyecto_final')
+
+""" IMPORTS FILES """
+import persistence
+import entities
+
+os.system("clear")
