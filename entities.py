60511488183aefab6e9cf3c9918b09402c97c8ee
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-05-31 10:42:58 -0300

    create classes and create object to webpage

--- entities.py
+++ entities.py
@@ -2,32 +2,16 @@
 """
 Made by Facundo Diaz - Tomas De Castro - Tadeo Grach for Holberton School 2021 """
 
-""" IMPORTS """
-import requests
-
-
 """OBJECTS DECLARATIONS"""
 
 class User:
-""" Class Users """
-    def __init__(self):
-        self.id_user = ""
-        self.name = ""
-        self.user = ""
-        self.password = ""
-        self.mail = ""
+    def __init__(self, name, mail):
+        self.id = None
+        self.name = name
+        self.mail = mail
 
 class Coin:
-""" Class Coin """
-    def __init__(self):
-        self.id_coin = 0
-        self.name = ""
-        self.price = 0
-        self.max_price
-
-class Suggest:
-""" Class Suggest """
-    def __init__(self):
-        self.id_suggest = 0
-        self.id_coin = 0
-        self.info = ""
+    def __init__(self, name, price, suggest):
+        self.name = name
+        self.price = price
+        self.suggest = suggest
--- web_dynamic/process.py
+++ web_dynamic/process.py
@@ -8,26 +8,9 @@ sys.path.append('/home/ubuntu/proyecto_final')
 
 """ IMPORTS FILES """
 import persistence
+import entities
 
-
-lasts_price_btc = persistence.traer_ultimos_precios_btc()
-suma = 0
-for i in lasts_price_btc:
-    suma = suma + int(i)
-print("la suma de todos los precios del btc en la ultima hora es", suma)
-print("promedio", suma/len(lasts_price_btc))
-
-lasts_price_doge = persistence.traer_ultimos_precios_doge()
-suma = 0
-for i in lasts_price_doge:
-    suma = suma + float(i)
-suma = int(suma)
-print("la suma de todos los precios del doge en la ultima hora es", suma)
-print("promedio", suma/len(lasts_price_doge))
-
-lasts_price_eth = persistence.traer_ultimos_precios_eth()
-suma = 0
-for i in lasts_price_eth:
-    suma = suma + int(i)
-print("la suma de todos los precios del eth en la ultima hora es", suma)
-print("promedio", suma/len(lasts_price_eth))
+precio_btc = persistence.traer_ultimo_precio_btc()
+name = "BTC"
+suggest = "va a subir"
+btc = entities.Coin(name, precio_btc, suggest)
