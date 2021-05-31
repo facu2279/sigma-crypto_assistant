0cecff81ce21dc73ea2ffebcaa1584995c1888ff
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-05-31 17:45:09 -0300

    fix

--- web_dynamic/process.py
+++ web_dynamic/process.py
@@ -2,18 +2,36 @@
 """
 Made by Facundo Diaz - Tomas De Castro - Tadeo Grach for Holberton School 2021 """
 
+""" colour background in hexa #262833 colour of logo in hexa #39E991 """
+
 """ IMPORTS EXTERN MODULES """
 import sys
 sys.path.append('/home/ubuntu/proyecto_final')
+import time
 
 """ IMPORTS FILES """
 import persistence
 import entities 
 import web_dynamic.mail as mail
+import info
+
+""" In case url 1 is not changing for these
+url = "https://api2.binance.com/"
+url = "https://api3.binance.com/"
+"""
+url = "https://api.binance.com/"
+i = 0
+
+while(1):
+        btc = info.consultar_precio_BTC(url)
+        persistence.save_price_bitcoin(btc)
+        eth = info.consultar_precio_ETH(url)
+        persistence.save_price_ethereum(eth)
+        doge = info.consultar_precio_DOGE(url)
+        persistence.save_price_doge(doge)
 
 precio_btc = persistence.traer_ultimo_precio_btc()
 name = "BTC"
-suggest = "va a subir"
+suggest = "el precio del " + name + " es " + btc
 btc = entities.Coin(name, precio_btc, suggest)
-resumen = "va subir el btc pa"
-mail.enviar_correos(resumen)
+mail.enviar_correos(suggest)
