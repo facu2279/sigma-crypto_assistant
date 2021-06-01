067622dc56f1ba2114c2ff5337acdbb0c613cc51
sigma123server <ubuntu@sigma.cryptocurrency.assistant@gmail.com>
2021-06-01 19:04:24 +0000

    Fix

--- web_dynamic/sigma_btc.py
+++ web_dynamic/sigma_btc.py
@@ -7,7 +7,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/bitcoin', strict_slashes=False)
+@app.route('/templates/bitcoin', strict_slashes=False)
 def bitcoin():
     """ """
     price = process.btc.price
@@ -15,12 +15,12 @@ def bitcoin():
     suggest = process.btc.suggest
     render_template('bitcoin.html', price=price, name=name, suggest=suggest)
 
-@app.route('/team', strict_slashes=False)
+@app.route('/templates/team', strict_slashes=False)
 def team():
     """ """
     render_template('team.html')
 
-@app.route('/index', strict_slashes=False)
+@app.route('/templates/index', strict_slashes=False)
 def index():
     """"""
     render_template('index.html')
