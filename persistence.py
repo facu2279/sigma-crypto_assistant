1872f7843c019f03810307f410dd78b95f711c8d
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-06-02 18:55:22 -0300

    Update persistence.py

--- persistence.py
+++ persistence.py
@@ -58,6 +58,7 @@ def traer_ultimo_precio_btc():
     consulta = newxd.cursor()
     consulta.execute("SELECT price FROM history_coin WHERE name='BTC' ORDER BY id DESC LIMIT 1;")
     resultado = consulta.fetchall()
+    price = 0
     for i in resultado:
         for i2 in i:
             price = i2
@@ -156,6 +157,7 @@ def traer_ultimo_precio_doge():
     consulta = newxd.cursor()
     consulta.execute("SELECT price FROM history_coin WHERE name='DOGE' ORDER BY id DESC LIMIT 1;")
     resultado = consulta.fetchall()
+    price = 0
     for i in resultado:
         for i2 in i:
             price = i2
@@ -178,6 +180,7 @@ def traer_masviejo_precio_doge():
     consulta = newxd.cursor()
     consulta.execute("SELECT price FROM history_coin WHERE name='DOGE' ORDER BY id ASC LIMIT 1;")
     resultado = consulta.fetchall()
+    price = 0
     for i in resultado:
         for i2 in i:
             price = i2
@@ -188,6 +191,7 @@ def traer_mayor_24_doge():
     consulta = newxd.cursor()
     consulta.execute("SELECT price FROM history_coin WHERE name='DOGE' ORDER BY price DESC LIMIT 1;")
     resultado = consulta.fetchall()
+    res = 0
     for i in resultado:
         for i2 in i:
             res = i2
@@ -198,6 +202,7 @@ def traer_menor_24_doge():
     consulta = newxd.cursor()
     consulta.execute("SELECT price FROM history_coin WHERE name='DOGE' ORDER BY price ASC LIMIT 1;")
     resultado = consulta.fetchall()
+    res = 0
     for i in resultado:
         for i2 in i:
             res = i2
@@ -253,6 +258,7 @@ def traer_ultimo_precio_eth():
     consulta = newxd.cursor()
     consulta.execute("SELECT price FROM history_coin WHERE name='ETH' ORDER BY id DESC LIMIT 1;")
     resultado = consulta.fetchall()
+    price = 0
     for i in resultado:
         for i2 in i:
             price = i2
@@ -275,6 +281,7 @@ def traer_masviejo_precio_eth():
     consulta = newxd.cursor()
     consulta.execute("SELECT price FROM history_coin WHERE name='ETH' ORDER BY id ASC LIMIT 1;")
     resultado = consulta.fetchall()
+    price = 0
     for i in resultado:
         for i2 in i:
             price = i2
@@ -285,6 +292,7 @@ def traer_mayor_24_eth():
     consulta = newxd.cursor()
     consulta.execute("SELECT price FROM history_coin WHERE name='ETH' ORDER BY price DESC LIMIT 1;")
     resultado = consulta.fetchall()
+    res = 0
     for i in resultado:
         for i2 in i:
             res = i2
@@ -295,6 +303,7 @@ def traer_menor_24_eth():
     consulta = newxd.cursor()
     consulta.execute("SELECT price FROM history_coin WHERE name='ETH' ORDER BY price ASC LIMIT 1;")
     resultado = consulta.fetchall()
+    res = 0
     for i in resultado:
         for i2 in i:
             res = i2
