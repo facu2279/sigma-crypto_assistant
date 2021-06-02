aee091a74a7849ae1f8bf81c01dfc0880caf27fd
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-06-02 16:08:49 -0300

    Update persistence.py

--- persistence.py
+++ persistence.py
@@ -225,12 +225,7 @@ def traer_ultimos_precios_eth():
 def insert_new_tendencia(name, average, min_price, max_price):
     nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
     consulta = nuevaconexion.cursor()
-    date = datetime.now()
-    date = str(date)
-    date = date[0:11]
-    print(date)
-    date = int(datetime.strptime(date, '%Y-%m-%d').strftime("%s"))
-    print(date)
+    date = 12345678912
     consulta.execute("INSERT INTO tendencias (name, average, max_price, min_price, dateprice) VALUES ('" + name  +"', '" + average +"', '" + max_price + "', '" + min_price + "', " + date + ");")
     nuevaconexion.commit()
 
