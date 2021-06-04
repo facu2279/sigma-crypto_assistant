62321479d78029b6138c0ea058e8736a999f1b89
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-06-04 13:04:03 -0300

    A

--- web_dynamic/entities.py
+++ web_dynamic/entities.py
@@ -2,6 +2,9 @@
 """
 Made by Facundo Diaz - Tomas De Castro - Tadeo Grach for Holberton School 2021 """
 
+""" IMPORTS """
+import persistence
+import percent
 """OBJECTS DECLARATIONS"""
 
 class User:
@@ -11,12 +14,8 @@ class User:
         self.mail = mail
 
 class Coin:
-    def __init__(self, name, price, porcenaje_24, mayor, menor):
-        self.name = name
-        self.price = price
-        self.porcentaje = porcenaje_24
-        self.mayor = mayor
-        self.menor = menor
+    def __init__(self, name):
+        self.refresh_coin(name)
 
     def to_dict(self):
         new = {}
@@ -26,3 +25,23 @@ class Coin:
         new["mayor"] = self.mayor
         new["menor"] = self.menor
         return new
+    
+    def refresh_coin(self, name):
+        if name == "BTC":
+            self.name = name
+            self.price = persistence.traer_ultimo_precio_btc()
+            self.porcentaje = percent.porcentaje_btc_24()
+            self.mayor = persistence.traer_mayor_24_btc()
+            self.menor = persistence.traer_mayor_24_btc()
+        elif name == "DOGE":
+            self.name = name
+            self.price = persistence.traer_ultimo_precio_doge()
+            self.porcentaje = percent.porcentaje_doge_24()
+            self.mayor = persistence.traer_mayor_24_doge()
+            self.menor = persistence.traer_mayor_24_doge()
+        elif name == "ETH":
+            self.name = name
+            self.price = persistence.traer_ultimo_precio_eth()
+            self.porcentaje = percent.porcentaje_eth_24()
+            self.mayor = persistence.traer_mayor_24_eth()
+            self.mayor = persistence.traer_mayor_24_eth()
--- web_dynamic/objects.py
+++ web_dynamic/objects.py
@@ -7,56 +7,8 @@ Made by Facundo Diaz - Tomas De Castro - Tadeo Grach for Holberton School 2021 "
 import persistence
 import entities 
 import percent
-import time
 
-""" 
-estructura objetos
 
-(name, price, porcenaje_24, mayor, menor)
-
-"""
-name = ""
-name2 = ""
-name3 = ""
-price = ""
-price2 = ""
-price3 = ""
-porcentaje = ""
-porcentaje2 = ""
-porcentaje3 = ""
-mayor = ""
-menor = ""
-mayor2 = ""
-menor2 = ""
-mayor3 = ""
-menor3 = ""
-
-def refresh_objects_values():
-    """ BTC """
-    global name, price, porcentaje, mayor, menor
-    global name2, price2, porcentaje2, mayor2, menor2
-    global name3, price3, porcentaje3, mayor3, menor3
-    name = "BTC"
-    price = persistence.traer_ultimo_precio_btc()
-    porcentaje = percent.porcentaje_btc_24()
-    mayor = persistence.traer_mayor_24_btc()
-    menor = persistence.traer_mayor_24_btc()
-
-    """ DOGE """
-    name2 = "DOGE"
-    price2 = persistence.traer_ultimo_precio_doge()
-    porcentaje2 = percent.porcentaje_doge_24()
-    mayor2 = persistence.traer_mayor_24_doge()
-    menor2 = persistence.traer_mayor_24_doge()
-
-    """ ETH """
-    name3 = "ETH"
-    price3 = persistence.traer_ultimo_precio_eth()
-    porcentaje3 = percent.porcentaje_eth_24()
-    mayor3 = persistence.traer_mayor_24_eth()
-    menor3 = persistence.traer_mayor_24_eth()
-
-refresh_objects_values()
-btc = entities.Coin(name, price, porcentaje, mayor, menor)
-doge = entities.Coin(name2, price2, porcentaje2, mayor2, menor2)
-eth = entities.Coin(name3, price3, porcentaje3, mayor3, menor3)
+btc = entities.Coin("BTC")
+doge = entities.Coin("DOGE")
+eth = entities.Coin("ETH")
