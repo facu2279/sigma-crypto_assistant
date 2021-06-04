ae3979c85ee230f11d6515d21df99a00140f2a85
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-06-04 13:58:57 -0300

    Update process.py

--- web_dynamic/process.py
+++ web_dynamic/process.py
@@ -34,6 +34,7 @@ while(i >= 0):
         percent.chequear_tendencias()
         percent.chequear_movimientos()
     print(i)
-    mail.daily_resume()
+    print(btc.porcentaje)
+    """mail.daily_resume()"""
     i = -1
 
