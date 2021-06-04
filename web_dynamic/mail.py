4349744a94fa6016a9fc494b3acb8ccb84460ece
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-06-04 13:33:26 -0300

    fix

--- web_dynamic/mail.py
+++ web_dynamic/mail.py
@@ -6,7 +6,7 @@ Made by Facundo Diaz - Tomas De Castro - Tadeo Grach for Holberton School 2021
 """ IMPORTS EXTERN MODULES """
 import os
 import time
-
+import smtplib
 """ IMPORTS FILES """
 import persistence
 
@@ -18,3 +18,26 @@ def enviar_correos(resumen):
         correo = str(users[i + 1])
         content = content + nombre + resumen
         os.system("echo -e 'Subject: subject \n\n " + content + "' | ssmtp " + correo)
+
+def daily_resume():
+    remitente = "Desde gnucita <sigma.cryptocurrency.assistant@gmail.com>" 
+    destinatario = "Mama de Gnucita <ddiiaazzfacu@gmail.com>" 
+    asunto = "E-mal HTML enviado desde Python" 
+    mensaje = """Hola!<br/> <br/> 
+    Este es un <b>e-mail</b> enviando desde <b>Python</b> 
+    """
+
+    email = """From: %s 
+    To: %s 
+    MIME-Version: 1.0 
+    Content-type: text/html 
+    Subject: %s 
+
+    %s
+    """ % (remitente, destinatario, asunto, mensaje) 
+    try: 
+        smtp = smtplib.SMTP('localhost') 
+        smtp.sendmail(remitente, destinatario, email) 
+        print("Correo enviado") 
+    except: 
+        print("ERROR")
--- web_dynamic/process.py
+++ web_dynamic/process.py
@@ -34,5 +34,6 @@ while(i >= 0):
         percent.chequear_tendencias()
         percent.chequear_movimientos()
     print(i)
+    mail.daily_resume()
     i = i + 1
 
