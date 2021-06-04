1ef5acb9513820209798b8b40b611af77054fea8
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-06-04 11:18:23 -0300

    Update objects.py

--- web_dynamic/objects.py
+++ web_dynamic/objects.py
@@ -17,34 +17,33 @@ estructura objetos
 """
 
 
-while(1):
-    """ BTC """
+""" BTC """
 
-    name = "BTC"
-    price = persistence.traer_ultimo_precio_btc()
-    porcentaje = percent.porcentaje_btc_24()
-    mayor = persistence.traer_mayor_24_btc()
-    menor = persistence.traer_mayor_24_btc()
+name = "BTC"
+price = persistence.traer_ultimo_precio_btc()
+porcentaje = percent.porcentaje_btc_24()
+mayor = persistence.traer_mayor_24_btc()
+menor = persistence.traer_mayor_24_btc()
 
-    btc = entities.Coin(name, price, porcentaje, mayor, menor)
-
+btc = entities.Coin(name, price, porcentaje, mayor, menor)
 
 
-    """ DOGE """
-    name2 = "DOGE"
-    price2 = persistence.traer_ultimo_precio_doge()
-    porcentaje2 = percent.porcentaje_doge_24()
-    mayor2 = persistence.traer_mayor_24_doge()
-    menor2 = persistence.traer_mayor_24_doge()
 
-    doge = entities.Coin(name2, price2, porcentaje2, mayor2, menor2)
+""" DOGE """
+name2 = "DOGE"
+price2 = persistence.traer_ultimo_precio_doge()
+porcentaje2 = percent.porcentaje_doge_24()
+mayor2 = persistence.traer_mayor_24_doge()
+menor2 = persistence.traer_mayor_24_doge()
+
+doge = entities.Coin(name2, price2, porcentaje2, mayor2, menor2)
 
 
-    """ ETH """
-    name3 = "ETH"
-    price3 = persistence.traer_ultimo_precio_eth()
-    porcentaje3 = percent.porcentaje_eth_24()
-    mayor3 = persistence.traer_mayor_24_eth()
-    menor3 = persistence.traer_mayor_24_eth()
+""" ETH """
+name3 = "ETH"
+price3 = persistence.traer_ultimo_precio_eth()
+porcentaje3 = percent.porcentaje_eth_24()
+mayor3 = persistence.traer_mayor_24_eth()
+menor3 = persistence.traer_mayor_24_eth()
 
-    eth = entities.Coin(name3, price3, porcentaje3, mayor3, menor3)
+eth = entities.Coin(name3, price3, porcentaje3, mayor3, menor3)
