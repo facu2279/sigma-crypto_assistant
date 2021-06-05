fac1d3631fd60aa4599c9021ad95218ae56c0281
sigma123server <ubuntu@sigma.cryptocurrency.assistant@gmail.com>
2021-06-05 16:48:38 +0000

    Add doge page and eth

--- web_dynamic/sigma_btc.py
+++ web_dynamic/sigma_btc.py
@@ -28,6 +28,26 @@ def bitcoin():
     percent = btc.porcentaje
     return render_template('bitcoin.html', price=price, max_price=max_price, min_price=min_price, percent=percent, eth_percent=eth.porcentaje, doge_percent=doge.porcentaje)
 
+@app.route('/ethereum', strict_slashes=False)
+def ethereum():
+    """ """
+    eth.refresh_coin(eth.name)
+    price = eth.price
+    max_price = eth.mayor
+    min_price = eth.menor
+    percent = eth.porcentaje
+    return render_template('ethereum.html', price=price, max_price=max_price, min_price=min_price, percent=percent, btc_percent=btc.porcentaje, doge_percent=doge.porcentaje)
+
+@app.route('/dogecoin', strict_slashes=False)
+def dogecoin():
+    """ """
+    doge.refresh_coin(doge.name)
+    price = doge.price
+    max_price = doge.mayor
+    min_price = doge.menor
+    percent = doge.porcentaje
+    return render_template('dogecoin.html', price=price, max_price=max_price, min_price=min_price, percent=percent, btc_percent=btc.porcentaje, eth_percent=eth.porcentaje)
+
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port='5000')
