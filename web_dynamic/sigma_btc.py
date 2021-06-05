7682ffd3cf7099a157c5d4f0f6337a13a3897b08
sigma123server <ubuntu@sigma.cryptocurrency.assistant@gmail.com>
2021-06-05 03:51:06 +0000

    add tadeo changes 0:54

--- web_dynamic/sigma_btc.py
+++ web_dynamic/sigma_btc.py
@@ -1,19 +1,32 @@
 #!/usr/bin/python3
 """ """
 
-import process
+from objects import btc, doge, eth
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+@app.route('/', strict_slashes=False)
+def index():
+    """"""
+    return render_template('index.html')
+
+@app.route('/team', strict_slashes=False)
+def team():
+    """"""
+    return render_template('team.html')
+
+
 @app.route('/bitcoin', strict_slashes=False)
 def bitcoin():
     """ """
-    price = process.doge.price
-    name = process.doge.name
-    suggest = process.doge.suggest
-    return render_template('bitcoin.html', price=price, name=name, suggest=suggest)
+    btc.refresh_coin(btc.name)
+    price = btc.price
+    max_price = btc.mayor
+    min_price = btc.menor
+    percent = btc.porcentaje
+    return render_template('bitcoin.html', price=price, max_price=max_price, min_price=min_price, percent=percent)
 
 if __name__ == "__main__":
     """ Main Function """
