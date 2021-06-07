97bc6bcdbe3544b965a20e28fbd566cf940c124b
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-06-07 11:02:41 -0300

    Update process.py

--- web_dynamic/process.py
+++ web_dynamic/process.py
@@ -26,14 +26,14 @@ while(i >= 0):
     persistence.save_price_ethereum(eth_price)
     doge_price = info.consultar_precio_DOGE(url)
     persistence.save_price_doge(doge_price)
-    percent.chequear_movimientos()
+    """percent.chequear_movimientos()
 
     if i % 60 == 0:
         percent.insert_in_tendencias()
     
     if i % 1440 == 0:
         mail.daily_resume()
-
+    """
     percent.insert_in_tendencias()
     print(i)
     i += 1    
