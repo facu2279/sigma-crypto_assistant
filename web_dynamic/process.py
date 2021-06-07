cc50863c995b44475308ab499472e1fd4154fe48
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-06-07 12:21:24 -0300

    Update process.py

--- web_dynamic/process.py
+++ web_dynamic/process.py
@@ -43,7 +43,7 @@ while(i >= 0):
     print(btc_tendencia.time)"""
     res = persistence.traer_tendencias_btc()
     for info in res:
-        new = entities.Tendencia(info[1],info[2],info[3],info[4],info[5],info[6],info[7])
+        new = entities.Tendencia(info[1], info[2], info[3], info[4], info[5], info[6], info[7])
         print(new, type(new))
     print("----------")
     print(i)
