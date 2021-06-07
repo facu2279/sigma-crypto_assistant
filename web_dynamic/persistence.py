a105752bb36008bb48ee3d196aac9904c1d2135e
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-06-07 11:01:53 -0300

    Update persistence.py

--- web_dynamic/persistence.py
+++ web_dynamic/persistence.py
@@ -383,7 +383,7 @@ def insert_new_tendencia(name, average, min_price, max_price, openprice, closepr
     nuevaconexion = MySQLdb.connect(host=MY_H, user=MY_U, passwd=MY_P, db=MY_D)
     consulta = nuevaconexion.cursor()
     date = str(datetime.now())
-    consulta.execute("INSERT INTO tendencias (name, average, max, min, open, close, date) VALUES ('" + name  +"', '" + average +"', '" + max_price + "', '" + min_price + "', '" + openprice + "', " + closeprice + "'," + date + "');")
+    consulta.execute("INSERT INTO tendencias (name, average, max, min, open, close, date) VALUES ('" + name  +"', '" + average +"', '" + max_price + "', '" + min_price + "', '" + openprice + "', " + closeprice + "','" + date + "');")
     nuevaconexion.commit()
 
 
