6102b17a25c4f391ce71e81f706e1c8e38fc2eea
facu2279 <70913784+facu2279@users.noreply.github.com>
2021-06-09 11:51:02 -0300

    Update process.py

--- web_dynamic/process.py
+++ web_dynamic/process.py
@@ -33,7 +33,11 @@ while(i >= 0):
     doge_price = info.consultar_precio_DOGE(url)
     persistence.save_price_doge(doge_price)
     a = percent.detectar_picos(a)
+    if a != 0:
+        cortarpicos = i + 180
     b = percent.chequear_movimientos(b)
+    if b != 0:
+        cortarconstantes = i + 180
 
     """ Save trends every hour """
     if i % 60 == 0:
@@ -41,22 +45,18 @@ while(i >= 0):
         print("Insert tendencia at", datetime.now())
 
     """ Every 3 hours clear history of constants and peaks """
-    if i % 180 == 0:
-        if a != 0:
-            a = 0
-            print("Clean mail history of 'detectar_picos'")
-        if b != 0:
-            b = 0
-            print("Clean mail history of 'chequear_movimientos'")
+    if cortarpicos % 180 == 0:
+        a = 0
+        print("Clean mail history of 'detectar_picos'")
 
-    """ Send daily summary 
+    if cortarconstantes % 180 == 0:
+        b = 0
+        print("Clean mail history of 'chequear_movimientos'")
+
+    """Send daily summary """
     if i % 1440 == 0:
         mail.daily_resume()
-        print("Sending daily resume at", datetime.now())"""
-    
-    if i % 240 == 0:
-        mail.daily_resume()
-        print("Sending daily resume test at", datetime.now())
+        print("Sending daily resume at", datetime.now())
 
     print("LAP: ", i, "--" , datetime.now())
     i += 1    
